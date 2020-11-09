308942be4f249b05d6b695fc7948713421faa6c0
Embryond <69889336+Embryond@users.noreply.github.com>
2020-11-09 13:04:08 +0700

    Add files via upload

---
+++ Nilai_17.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilai_17;
+
+/**
+ *
+ * @author User
+ */
+public class Nilai_17 {
+     public int getnilaiPengetahuan (int pengetahuan){
+        return pengetahuan;
+    }
+
+    public int getnilaiKeterampilan (int keterampilan){
+        return keterampilan;
+    }
+    
+}
